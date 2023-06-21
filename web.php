42fafa96e0fd71d67cda3a5ad60463adbaf26415
ryosuke <ryosukekdr@yahoo.co.jp>
2023-06-21 05:51:00 +0000

    課題提出

---
+++ web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add')->middleware('auth');
+});
+
+
+
+//課題で追記
+use App\Http\Controllers\Admin\AAAController;
+Route::controller(AAAController::class)->group(function() {
+    Route::get('xxx', 'bbb');
+});
+
+//課題で追記
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add')->middleware('auth');  //リダイレクト設定を課題で追記
+    Route::get('profile/edit', 'edit')->middleware('auth');   //リダイレクト設定を課題で追記
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
